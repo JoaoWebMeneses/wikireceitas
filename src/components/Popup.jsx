a8746e91a5e67ac1be8f35347051482405f4e1e4
JoaoWebMeneses <joaowebmeneses@gmail.com>
2023-12-16 09:44:05 -0300

    Terminando wikireceitas

---
+++ src/components/Popup.jsx
@@ -0,0 +1,11 @@
+import "./Popup.css"
+
+function Popup(){
+    return(
+        <div className="popup">
+           Postado com sucesso
+        </div>
+    )
+}
+
+export default Popup
--- src/routes/CriarReceitas.jsx
+++ src/routes/CriarReceitas.jsx
@@ -3,56 +3,79 @@ import { getFirestore, getDocs, collection, addDoc } from "firebase/firestore";
 import { initializeApp } from "firebase/app";
 import "./CriarReceitas.css";
 import { BsSend } from "react-icons/bs";
+import Popup from "../components/Popup";
 
 const firebaseApp = initializeApp({
-    apiKey: "AIzaSyDNP_dhg5VIdIhEXacvUY5dvX_XIiJg3v4",
-    authDomain: "wikireceitas-6a0f0.firebaseapp.com",
-    projectId: "wikireceitas-6a0f0",
-})
+  apiKey: "AIzaSyDNP_dhg5VIdIhEXacvUY5dvX_XIiJg3v4",
+  authDomain: "wikireceitas-6a0f0.firebaseapp.com",
+  projectId: "wikireceitas-6a0f0",
+});
 
 const CriarReceitas = () => {
-    const [ingredientes, setIngredientes] = useState("");
-    const [preparo, setPreparo] = useState("");
-    const [titulo, setTitulo] = useState("");
-    const [url, setUrl] = useState("");
-    const [receitas, setReceitas] = useState([]);
+  const [ingredientes, setIngredientes] = useState("");
+  const [preparo, setPreparo] = useState("");
+  const [titulo, setTitulo] = useState("");
+  const [url, setUrl] = useState("");
+  const [receitas, setReceitas] = useState([]);
+  const [showPopup, setShowPopup] = useState(false);
 
-    const db = getFirestore(firebaseApp);
-    const receitaCollectionRef = collection(db, 'receitas')
+  const db = getFirestore(firebaseApp);
+  const receitaCollectionRef = collection(db, "receitas");
 
-    async function createReceita() {
-        const receita = await addDoc(receitaCollectionRef, {
-            titulo,
-            ingredientes,
-            preparo,
-            url,
-        });
-        console.log(receita);
-    }
+  async function createReceita() {
+    const receita = await addDoc(receitaCollectionRef, {
+      titulo,
+      ingredientes,
+      preparo,
+      url,
+    });
+    console.log(receita);
+    setShowPopup(true);
+  }
 
-    useEffect(() => {
-        const getReceitas = async () => {
-            const data = await getDocs(receitaCollectionRef);
-            setReceitas(data.docs.map((doc) => ({ ...doc.data(), id: doc.id })));
-        };
-        getReceitas();
-    }, []);
+  useEffect(() => {
+    const getReceitas = async () => {
+      const data = await getDocs(receitaCollectionRef);
+      setReceitas(data.docs.map((doc) => ({ ...doc.data(), id: doc.id })));
+    };
+    getReceitas();
+  }, []);
 
-    return (
-        <div>
-            <h1>Crie sua receita</h1>
-            <textarea placeholder="Titulo" value={titulo} onChange={(e) => setTitulo(e.target.value)}></textarea>
-            <br/>
-            <textarea placeholder="Ingredientes" value={ingredientes} onChange={(e) => setIngredientes(e.target.value)}></textarea>
-            <br/>
-            <textarea placeholder="Modo de Preparo" value={preparo} onChange={(e) => setPreparo(e.target.value)}></textarea>
-            <br/>
-            <textarea placeholder="Url da imagem de sua comida" value={url} onChange={(e) => setUrl(e.target.value)}/>
-            <br/>
-            <br/>
-            <button onClick={createReceita}><BsSend /></button>
-        </div>
-    )
-}
+  return (
+    <div>
+      <h1>Crie sua receita</h1>
+      <textarea
+        placeholder="Titulo"
+        value={titulo}
+        onChange={(e) => setTitulo(e.target.value)}
+      ></textarea>
+      <br />
+      <textarea
+        placeholder="Ingredientes"
+        value={ingredientes}
+        onChange={(e) => setIngredientes(e.target.value)}
+      ></textarea>
+      <br />
+      <textarea
+        placeholder="Modo de Preparo"
+        value={preparo}
+        onChange={(e) => setPreparo(e.target.value)}
+      ></textarea>
+      <br />
+      <textarea
+        placeholder="Url da imagem de sua comida"
+        value={url}
+        onChange={(e) => setUrl(e.target.value)}
+      />
+      <br />
+      <br />
+      <button onClick={createReceita}>
+        <BsSend />
+      </button>
+
+      {showPopup && <Popup />}
+    </div>
+  );
+};
 
 export default CriarReceitas;
